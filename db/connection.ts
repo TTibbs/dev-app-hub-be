ad30811b078fa3bc49217f3a7a3adf8dcd049eb3
TTibbs <terryjw.97@gmail.com>
2025-03-19 05:24:38 +0000

    Fixed package scripts and connection file to db

--- db/connection.ts
+++ db/connection.ts
@@ -1,31 +1,20 @@
-import { Pool } from "pg";
-import { PoolConfig } from "./types";
-import path from "path";
-import fs from "fs";
+import { Pool, PoolConfig } from "pg";
 const ENV = process.env.NODE_ENV || "development";
 
-if (!ENV) {
-  throw new Error("NODE_ENV is not set");
-}
-
-// Check if we're running from the dist folder
-const envPath = fs.existsSync(path.join(__dirname, `../.env.${ENV}`))
-  ? path.join(__dirname, `../.env.${ENV}`) // When running from dist
-  : path.join(__dirname, `/../.env.${ENV}`); // When running from root
-
 require("dotenv").config({
-  path: envPath,
+  path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PG database or database URL not set");
+  throw new Error("PG databse or database URL not set");
 }
 
-const config: PoolConfig = {};
+const config: PoolConfig = {
+  connectionString: process.env.DATABASE_URL as string,
+};
 
 if (ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL as string;
   config.max = 2;
 }
 
-export default new Pool(config as any);
+export default new Pool(config);
